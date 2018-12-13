42104b085b86d70239f665a13f8b0588d78ae126
kashifm <kashif_mushtaq@trendmicro.com>
2018-12-13 08:38:34 -0500

    correct jdk path

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-rm -r ./build
-mkdir -m777 ./build
+
+#correct me before execution
+export PATH="/c/Program Files/Java/jdk1.8.0_162/bin"
+
+/bin/rm -r ./build
+/bin/mkdir -p build
 javac -cp ./lib/gson-2.8.5.jar -d ./build ./src/*.java
 cd ./build
 jar -cvfe NeoBrowser.jar *.class
